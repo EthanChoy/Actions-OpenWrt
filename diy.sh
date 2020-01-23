5c8a7b164bf4bc4f8ceb8d30293c27517c729747
EthanChoy <42546559+EthanChoy@users.noreply.github.com>
2020-01-23 14:33:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/happyzhang1995/openwrt-adguardhome.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/jerrykuku/luci-theme-argon.git
