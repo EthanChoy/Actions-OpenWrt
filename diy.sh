865624eaf74b3f84d856e191558e7449ee367b33
EthanChoy <42546559+EthanChoy@users.noreply.github.com>
2020-03-12 09:51:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,4 @@
 # git clone https://github.com/happyzhang1995/openwrt-adguardhome.git
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 # git clone https://github.com/jerrykuku/luci-theme-argon.git
+sed -i 's/192.168.1.1/192.168.2.22/g' package/base-files/files/bin/config_generate
